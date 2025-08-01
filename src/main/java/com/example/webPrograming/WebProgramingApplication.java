dc44791a4c8a2ee08ed769cf9452b883809546bb
sunho1025 <sdu10722@gmail.com>
2025-08-01 15:15:02 +0900

    JSP 설정 변경

---
+++ src/main/java/com/example/webPrograming/WebProgramingApplication.java
@@ -0,0 +1,13 @@
+package com.example.webPrograming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgramingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProgramingApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <div>${name}</div>
+        <div><%=1+2%></div>
+    </body>
+</html>
+
